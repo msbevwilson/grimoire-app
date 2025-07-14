acb8f72784c0de7ef1b2a95d560895c49aaeae4c
msbevwilson <82551832+msbevwilson@users.noreply.github.com>
2025-07-14 19:20:08 -0400

    initial commit

---
+++ Back/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+//app.use(helmet());
+app.use(express.json());
+//app.use(cookieParser());
+mongoose.connect('mongodb+srv://beverlyDBUser:vnuFPUB63gYQWst@cluster0.p80kybq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+// Headers & CORS
+const helmet = require('helmet')
+
+app.use(helmet({crossOriginResourcePolicy: false,}))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next();
+})
+
+// ROUTER
+const bookRoutes = require('./routes/book.routes')
+app.use('/api/books', bookRoutes)
+const userRoutes = require('./routes/user.routes')
+app.use('/api/auth', userRoutes)
+
+// static path for images
+const path = require('path')
+app.use('/images', express.static(path.join(__dirname, 'images'))) 
+
+module.exports = app
